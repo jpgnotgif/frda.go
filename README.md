fd1b115a7c28f1b08db70123cfe39f2475ad2689
Joe Gutierrez <joe@brightroll.com>
2016-01-23 21:48:45 -0800

    1st commit

---
+++ README.md
@@ -0,0 +1,69 @@
+# fg-frames.go
+A golang based, http web service that provides fighting game character
+frame data. A proof of concept service built as a means to explore
+golang and various http tool kits. This service only hosts Ultra Street
+Fighter 4 version 1.05 data at the moment, but will be expanded to
+include other games in the future.
+
+## Data sources
+- [eventhubs](http://www.eventhubs.com/moves/sf4/)
+- [wiki.shoryuken](http://wiki.shoryuken.com/Ultra_Street_Fighter_IV)
+
+The compiled data used by this service can be found here
+- [source](https://github.com/jpgnotgif/frame-data)
+
+## Bootstrap
+- [Install golang](https://golang.org/doc/install)
+- Build it
+
+```
+$ go install github.com/jpgnotgif/fgframes
+$ GOPATH/bin/fgframes
+```
+
+## JSON Schema
+```
+{
+  normal-attack: {
+    standing: { startup: num, active: num, recovery: num,
+block-advantage: num, hit-advantage: num }
+    close standing: {...}
+    crouching: {...}
+    neutral jump: {...}
+    diagonal jump: {...}
+  }
+  unique-attack: {...}
+}
+```
+
+## Attack Legend
+Notation|Expanded
+--------|--------
+lp|Light Punch / Jab
+mp|Medium Punch / Strong
+hp|Hard Punch / Fierce
+lk|Light Kick / Short
+mk|Medium Kick / Forward
+hk|Hard Kick / Roundhouse
+
+## Character State Legend
+Notation|Expanded
+--------|-------
+s|Standing
+cs|Close Standing
+cr|Crouching
+nj|Neutral Jump / Jump Up
+dj|Diagonal Jump / Jump Toward
+
+## Frame Data Legend
+Notation|Expanded
+--------|--------
+s|Startup
+a|Active
+r|Recovery
+ba|Block Advantage
+ha|Hit Advantage
+
+## Tech
+- [golang](https://golang.org/)
+- [gin gonic](https://gin-gonic.github.io/gin/)
---
+++ fgframes.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"io/ioutil"
+	"path/filepath"
+	"strings"
+)
+
+const BasePath = "data/title"
+
+type TitleMap struct {
+	name         string
+	characterMap map[string]Character
+}
+
+type Character struct {
+	name string
+	data string
+}
+
+func BuildData(title string) map[string]Character {
+	dataMap := make(map[string]Character)
+
+	titleMap := map[string]bool{
+		"usf4": true,
+		"sfv":  true,
+	}
+	_, ok := titleMap[title]
+
+	if !ok {
+		return dataMap
+	}
+
+	names := CharacterNames(title)
+	for _, name := range names {
+		_, data := FindData(title, name)
+		dataMap[name] = Character{name, data}
+	}
+	return dataMap
+}
+
+func CharacterNames(title string) []string {
+	path := BasePath + "/" + title + "/" + "json"
+	names := []string{}
+	files, _ := ioutil.ReadDir(path)
+	for _, file := range files {
+		baseName := file.Name()
+		name := strings.TrimSuffix(baseName, filepath.Ext(baseName))
+		names = append(names, name)
+	}
+	return names
+}
+
+func FindData(title string, name string) (bool, string) {
+	path := BasePath + "/" + title + "/" + "json" + "/" + name + ".json"
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		return false, "Character not found"
+	}
+	d := string(data)
+	return true, d
+}
+
+func main() {
+	router := gin.Default()
+	usf4Map := TitleMap{name: "usf4", characterMap: BuildData("usf4")}
+
+	router.GET("/:title/:name", func(context *gin.Context) {
+		switch title := context.Param("title"); title {
+		case "usf4":
+			character, characterFound := usf4Map.characterMap[context.Param("name")]
+			if !characterFound {
+				context.JSON(404, gin.H{
+					"message": "character not found",
+				})
+			}
+			context.String(200, character.data)
+		case "sfv":
+			context.JSON(501, gin.H{
+				"message": "coming soon",
+			})
+		default:
+			context.JSON(404, gin.H{
+				"message": "title not found",
+			})
+		}
+
+	})
+	router.Run(":8080")
+}
